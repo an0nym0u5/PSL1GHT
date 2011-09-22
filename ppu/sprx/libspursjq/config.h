966cca385c7bd9e3afbfb95a692af50d80385ed8
an0nym0us <an0nym0us@privacy4.me>
2011-09-22 15:22:09 -0400

    added libspurs, libspursjq, and fixed libsysmodule exports.h

---
+++ ppu/sprx/libspursjq/config.h
@@ -0,0 +1,5 @@
+#define LIBRARY_NAME		"cellSpursJq"
+#define LIBRARY_SYMBOL		cellSpursJq
+
+#define LIBRARY_HEADER_1	0x2c000001
+#define LIBRARY_HEADER_2	0x0009
--- ppu/sprx/libsysmodule/exports.h
+++ ppu/sprx/libsysmodule/exports.h
@@ -7,8 +7,8 @@ EXPORT(sysModuleFinalize, 0x96c07adf);
 
 EXPORT(sysModuleLoad, 0x32267A31);
 EXPORT(sysModuleUnload, 0x112A5EE9);
-
 EXPORT(sysModuleIsLoaded, 0x5a59e258);
+
 EXPORT(sysModuleGetImagesize, 0x1ef115ef);
 EXPORT(sysModuleFetchImage, 0x3c92be09);
 EXPORT(sysModuleSetMemcontainer, 0xa193143c);
